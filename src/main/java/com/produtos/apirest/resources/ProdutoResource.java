896389d4b08bf941aaecce2fcc5c60078369382a
Michel Freitas <michelfreitas85@outlook.com>
2020-10-12 14:56:04 -0300

    Função de deletar e alterar produto

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -3,9 +3,11 @@ package com.produtos.apirest.resources;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,16 @@ public class ProdutoResource {
 	
 	@PostMapping("/produtos")
 	public Produto salvarProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produtos")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+	
+	@PutMapping("/produtos")
+	public Produto atualizaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 }
